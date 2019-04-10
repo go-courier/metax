c40cc1660bf5e5d241fdacb1a5b75ac6524fe75c
Morlay <morlay.null@gmail.com>
2019-04-10 17:52:14 +0800

    api fix

--- ctx.go
+++ ctx.go
@@ -6,6 +6,11 @@ type Ctx struct {
 	ctx context.Context
 }
 
+func (c Ctx) WithContext(ctx context.Context) Ctx {
+	c.ctx = ctx
+	return c
+}
+
 func (c Ctx) Context() context.Context {
 	if c.ctx == nil {
 		return context.Background()
